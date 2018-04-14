9e47aa958a88f996141d5e4232242e70535e74fa
SirawichDev <sirawit0676@gmail.com>
2018-04-14 09:02:34 +0700

    swap color with if condition

--- js/basic.js
+++ js/basic.js
@@ -1,6 +1,12 @@
 $(document).ready(function(){
+    var i =0;
     $("button").click(function () {
+        if( i==0){
         $("h1.hello").css("color","red");
-      });
+        i++;
+        }
+        else
+        $("h1.hello").css("color","blue");
+    });
   
 });
