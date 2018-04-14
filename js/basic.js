5f6e1020b8c8975f65db1ac3e9316c0dea455f44
SirawichDev <sirawit0676@gmail.com>
2018-04-14 09:33:33 +0700

    hide img

--- js/basic.js
+++ js/basic.js
@@ -1,8 +1,19 @@
-$(document).ready(function(){
+/* $(document).ready(function(){
     var i =0;
     $("button").click(function () {
         
         $("h1.hello").css("color","red");
       });
   
+}); */
+$(function(){
+    $(".image").click(function () {  
+        $(".image").hide();
+    });
+});
+
+$(function(){
+    $("button").click(function () {  
+        $("h1").css("color","pink");
+    });
 });
