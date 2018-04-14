c3eecd37cd776994fe43d19b2bf04a64b7e5c986
SirawichDev <sirawit0676@gmail.com>
2018-04-14 09:25:22 +0700

    added picture

--- js/basic.js
+++ js/basic.js
@@ -1,12 +1,8 @@
 $(document).ready(function(){
     var i =0;
     $("button").click(function () {
-        if( i==0){
+        
         $("h1.hello").css("color","red");
-        i++;
-        }
-        else
-        $("h1.hello").css("color","blue");
-    });
+      });
   
 });
